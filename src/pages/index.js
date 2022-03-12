0459498dc2a2a9d917386c531f7ce5935c06f35e
Mario Andres <marioarendon2@gmail.com>
2022-03-12 12:21:56 -0600

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import App from "../components/app"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <div>lol</div>
+    <App />
   </Layout>
 )
 
