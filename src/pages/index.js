ac6d794f5465d5c610a579c604df51da94e39561
Mario Andres <marioarendon2@gmail.com>
2022-03-15 23:07:06 -0600

    remove seo component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
 import App from "../components/app"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
     <App />
   </Layout>
 )
