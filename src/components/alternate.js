04243b650ea4c7ab52e21a1ea3cd50fae5927d27
Mario Andres <marioarendon2@gmail.com>
2022-03-17 20:08:22 -0600

    Calculate correctly alternate number

--- src/components/alternate.js
+++ src/components/alternate.js
@@ -7,47 +7,63 @@ const mapGeoSectToNumber = {
   SW: "400",
 }
 
+const addLeadingZero = num => {
+  return num < 10 ? `0${num}` : num
+}
+
 export const createAlternateNumbers = (township, quaterC) => {
   const townshipData = getRows(township)
   const quaterCData = getRows(quaterC)
 
   const townPartial = townshipData
     .filter(({ GRID }) => GRID)
-    .map(({ PPID, TWP, GRID, TWPUID }) => ({
-      PPID,
-      TWPUID,
+    .map(({ TWP, GRID, RGE }) => ({
       TWP,
+      RGE,
       GRID: `000${GRID}`,
     }))
 
-  const quaterPartial = quaterCData.map(({ QSECT, PTWPPPID, PTWP }) => ({
+  const quaterPartial = quaterCData.map(({ PSECT, QSECT, PTWP, PRGE }) => ({
+    PSECT: addLeadingZero(PSECT),
     QSECT: mapGeoSectToNumber[QSECT],
-    PTWPPPID,
     PTWP,
+    PRGE,
   }))
 
-  const alternateNumbers = townPartial.map(({ PPID, TWP, GRID, TWPUID }) => {
-    const quater = quaterPartial.find(
-      ({ PTWPPPID, PTWP }) => PTWPPPID === PPID && PTWP === TWP
-    )
+  const alternateNumbers = townPartial
+    .map(({ TWP, GRID, RGE }) => {
+      const quarter = quaterPartial
+        .filter(({ PRGE, PTWP }) => RGE === PRGE && TWP === PTWP)
+        .map(({ PSECT, QSECT }) => ({
+          GRID,
+          TWP,
+          RGE,
+          PSECT,
+          QSECT,
+          alternateNumber: `${GRID}${PSECT}${QSECT}`,
+        }))
 
-    return {
-      TWPUID,
-      TWP,
-      QSECT: quater.QSECT,
-      alternateNumber: `${GRID}${TWP}${quater.QSECT}`,
-    }
-  })
+      return quarter
+    })
+    .flat()
 
   return {
     columns: [
       {
-        Header: "TWPUID",
-        accessor: "TWPUID",
+        Header: "GRID",
+        accessor: "GRID",
+      },
+      {
+        Header: "RGE",
+        accessor: "RGE",
       },
       {
         Header: "TWP",
         accessor: "TWP",
+      },
+      {
+        Header: "PSECT",
+        accessor: "PSECT",
       },
       {
         Header: "QSECT",
